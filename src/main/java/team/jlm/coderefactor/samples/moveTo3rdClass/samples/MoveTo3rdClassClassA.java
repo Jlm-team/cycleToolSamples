3460dc17f1ba76052182901cfb6291f7b1a095bc
XYZboom <3242304316@qq.com>
2023-06-15 23:01:15 +0800

    feature(add moveTo3rdClass): add moveTo3rdClass

---
+++ src/main/java/team/jlm/coderefactor/samples/moveTo3rdClass/samples/MoveTo3rdClassClassA.java
@@ -0,0 +1,8 @@
+package team.jlm.coderefactor.samples.moveTo3rdClass.samples;
+
+public class MoveTo3rdClassClassA {
+    public void funcInA(MoveTo3rdClassClassB moveTo3rdClassClassB) {
+        System.out.println(moveTo3rdClassClassB);
+        System.out.println("funcInA");
+    }
+}
---
+++ src/main/java/team/jlm/coderefactor/samples/moveTo3rdClass/samples/MoveTo3rdClassClassB.java
@@ -0,0 +1,8 @@
+package team.jlm.coderefactor.samples.moveTo3rdClass.samples;
+
+public class MoveTo3rdClassClassB {
+    public int funcInB(MoveTo3rdClassClassA moveTo3rdClassClassA) {
+        System.out.println(moveTo3rdClassClassA);
+        return 0;
+    }
+}
