09422f2e668784454538349d92e0a5f96902c690
XYZboom <3242304316@qq.com>
2023-06-15 23:17:22 +0800

    feature(add unused import): add unused import

---
+++ src/main/java/team/jlm/coderefactor/samples/unusedimport/sample1/UnusedImportClassA.java
@@ -0,0 +1,7 @@
+package team.jlm.coderefactor.samples.unusedimport.sample1;
+
+import team.jlm.coderefactor.samples.unusedimport.sample1.inside.UnusedImportClassB;
+
+public class UnusedImportClassA {
+    UnusedImportClassB unusedImportClassB;
+}
---
+++ src/main/java/team/jlm/coderefactor/samples/unusedimport/sample1/inside/UnusedImportClassB.java
@@ -0,0 +1,7 @@
+package team.jlm.coderefactor.samples.unusedimport.sample1.inside;
+
+import team.jlm.coderefactor.samples.unusedimport.sample1.UnusedImportClassA;
+
+public class UnusedImportClassB {
+    // do sth.
+}
